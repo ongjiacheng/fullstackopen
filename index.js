609d172bd77ad08fa1b2320b7e87d464a5c84377
Ong Jia Cheng <ongjiacheng@Ongs-MacBook-Pro.local>
2025-05-21 23:13:08 +0800

    With frontend

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+
 const app = express()
 
 let notes = [
@@ -20,6 +21,7 @@ let notes = [
 ]
 
 app.use(express.json())
+app.use(express.static('dist'))
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
@@ -73,7 +75,7 @@ app.delete('/api/notes/:id', (request, response) => {
   response.status(204).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
